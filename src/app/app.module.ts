e4ceea4ad191abed44597928fe9a421bba49cd7b
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-12-15 17:56:43 -0300

    fixar menu e adicionar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { SendSmsComponent } from './send-sms/send-sms.component';
 import { SearchComponent } from './search/search.component';
@@ -12,7 +12,7 @@ import { SearchComponent } from './search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    FooterComponent,
     AboutComponent,
     SendSmsComponent,
     SearchComponent
