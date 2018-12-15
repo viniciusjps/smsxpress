89582afbf50fa338f761fa9e48ea09c8abd323b8
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-12-15 17:32:15 -0300

    adicionar consulta por protocolo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,62 @@ export class AppComponent {
    * Scroll To
    * @param id Target ID
    */
-  public scroll(id) {
+  public scroll(id): void {
     const target = document.getElementById(id);
     target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  public consult(value): void {
+    swal({
+      text: 'Informe o número do protocolo',
+      content: {
+        element: 'input',
+        attributes: {
+          placeholder: '0000-1111-2222'
+        }
+      },
+      icon: 'info',
+      button: {
+        text: 'Consultar',
+        closeModal: false,
+        className: 'ui green button'
+      }
+    })
+      .then(protocol => {
+        if (!protocol) {
+          throw null;
+        }
+        return fetch('https://api-smsxpress.herokuapp.com/api/sms/' + protocol);
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        let text = 'Mensagem: ' + json.text + '\n\n';
+        text += 'Destinatários: \n';
+        json.to.map(phone => {
+          text += phone + '\n';
+        });
+        const date = new Date(json.createdAt);
+        text += '\n' + 'Enviado em ' + date.toLocaleDateString();
+        swal({
+          title: 'Nº ' + json.protocol,
+          text: text,
+          button: {
+            text: 'fechar',
+            closeModal: true,
+            className: 'ui blue button'
+          }
+        });
+      })
+      .catch(error => {
+        if (error) {
+          swal('Ops', 'Verifique o número do seu protocolo', 'error');
+        } else {
+          swal.stopLoading();
+          swal.close();
+        }
+      });
   }
 
 }
