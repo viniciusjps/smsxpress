422eb83fb0555b3476b286fa3001aa1f5fd8a868
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-12-16 13:41:07 -0300

    agora vai, deploy heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/smsxpress'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/smsxpress/index.html'));
